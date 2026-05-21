b7f2b57f1706703e730666f15469f2366486c7d0
pablo-osuna-vargas <pabloosunavargas@gmail.com>
2026-05-21 11:00:39 -0300

    practica 19

---
+++ clase-07/practica-19/js/script.js
@@ -0,0 +1,23 @@
+let titulo = document.querySelector("h1");
+titulo.addEventListener("click", function() {
+	titulo.style.color = "red"});
+
+let subTitulo = document.querySelector("h2");
+subTitulo.addEventListener("click", function() {
+	subTitulo.style.color = "red"});
+
+let parrafo1 = document.querySelector(".uno");
+parrafo1.addEventListener("click", function() {
+	parrafo1.style.color = "red"});
+
+let parrafo2 = document.querySelector(".dos");
+parrafo2.addEventListener("click", function() {
+	parrafo2.style.color = "red"});
+
+let lista = document.querySelector("ul");
+lista.addEventListener("click", function() {
+	lista.style.color = "red"});
+
+let imagen = document.querySelector(".imagen");
+imagen.addEventListener("click", function() {
+	imagen.style.display = "none"});
